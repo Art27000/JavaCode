9dc947a56a7aecc962d845a034148f323b231a4e
Art2700 <blackatik27@gmail.com>
2025-03-25 21:22:26 +0300

    second commit

---
+++ task1.java
@@ -0,0 +1,10 @@
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class task1 {
+    public static void main(String[] args) {
+        System.out.println("My first program");
+    }
+}
